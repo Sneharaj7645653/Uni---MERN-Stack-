47b68fd482bb47be9e9a2574a6a441681ef709eb
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-08-13 15:09:27 +0530

    routing

--- Class 2/index.js
+++ Class 2/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//dummy data base
+let courses= [
+    {id:1 , name:'Java'},
+    {id:2 , name:'DBMS'},
+    {id:3 , name:'JavaScript'},
+]
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -11,6 +17,32 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.send('Contact Page');
 });
+
+
+app.get('/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/courses/:id',(req, res)=>{
+    console.log(req.params);
+    console.log(req.params.id);
+    console.log(courses);
+    let course = courses.filter((course) => {
+        return course.id == req.params.id;
+    })[0];
+
+    let course2 = courses.find((course) => {
+        return course.id == req.params.id;
+    });
+    res.send(course);
+
+    //cannot spend two responses
+    // res.send(course2);
+})
+
+
+
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
